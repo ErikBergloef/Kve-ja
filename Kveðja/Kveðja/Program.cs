681e3af7b0bf43c3c3e1a0cea1d4e60c6c42bc57
ErikBergloef <47384098+ErikBergloef@users.noreply.github.com>
2024-05-10 08:50:50 +0200

    Add files via upload

---
+++ Kveðja/Kveðja/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string signature = "Kvedja,\n";
+            
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine(signature + firstName);
+            
+
+
+        }
+    }
+}
